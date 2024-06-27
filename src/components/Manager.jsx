27ba48bf7f8d5c5db77e71628ff05baa58d9a101
Atharv Darekar <atharvd1893@gmail.com>
2024-06-27 11:40:35 +0530

    form saving

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 const Manager = () => {
+    const [form, setform] = useState({site: '', username: '', password: ''})
+    const [passwordArray, setpasswordArray] = useState([])
+    
+    
+    
+
+    const savePassword = () => {
+      console.log(form);
+     
+    }
+    
+
+    const handleChange = (e) => {
+      setform({...form,[e.target.name]: e.target.value})
+    }
+    
+
+    
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
 
             <div className="container mx-auto">
 
@@ -16,31 +34,42 @@ const Manager = () => {
                 <div className="flex flex-col p-4 text-black gap-8 items-center">
 
                     <input 
-                    className="rounded-full border border-green-500 w-full px-3 py-1" 
+                    className="rounded-full border border-green-500 w-full px-3 py-1"
+                    value = {form.site}
                     type="text" 
-                    name="" 
+                    name="site" 
                     id="" 
                     placeholder='Enter Website URL'
+                    onChange={handleChange}
                     />
                     <div className="flex w-full justify-between gap-8">
 
                         <input
                         type = "text"
+                        value = {form.username}
                         className="rounded-full border border-green-500 w-full px-3 py-1"
                         placeholder='Enter Username'
+                        name = "username"
+                        onChange={handleChange}
                         />
                             <div className="relative">
                             
                         <input 
                         type="password" 
+                        value = {form.password} 
                         className="rounded-full border border-green-500 w-full px-3 py-1" 
                         placeholder='Enter Password'
+                        name = "password"
+                                        onChange={handleChange}
+                        
                         />
                         <span className="absolute right-0"></span>
                         </div>
 
                     </div>
-                    <button className="flex items-center gap-2 bg-green-500 rounded-full px-5 py-2 w-fit hover:bg-white hover:text-green-500 hover: border-2 border-green-800" >
+                    <button onClick = {savePassword} 
+                    className="flex items-center gap-2 bg-green-500 rounded-full px-5 py-2 w-fit hover:bg-white hover:text-green-500 hover: border-2 border-green-800" 
+                    >
                         <lord-icon
                             src="https://cdn.lordicon.com/jgnvfzqg.json"
                             trigger="hover">
@@ -51,6 +80,9 @@ const Manager = () => {
 
         </>
     )
-}
+};
 
 export default Manager
+
+
+
